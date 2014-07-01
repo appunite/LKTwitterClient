0da15c5b35b4e64a5036a7db76e25201bab80b3d
Dev <dev@Dev-MacBook-Pro.local>
2014-07-01 13:20:19 +0200

    Initial Commit

---
+++ FunFunFun/LKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LKAppDelegate.h
+//  FunFunFun
+//
+//  Created by Dev on 7/1/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FunFunFun/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FunFunFun
+//
+//  Created by Dev on 7/1/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LKAppDelegate class]));
+    }
+}
